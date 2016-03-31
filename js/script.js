cfed774eb1c2a91b02615fe32afb825953e34ba1
Jason King <kingjason22@comcast.net>
2016-03-31 19:13:50 -0400

    added jquery

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 $(document).ready(function() {
-
 	$(function() {
 		$('[data-toggle="tooltip"]').tooltip()
 	});
+	$('.fadein').fadeIn(2000);
+	   
+	var fadeStart=10,
+		fadeUntil=300,
+		fadeout = $('.fadeout');
 
+	$(window).bind('scroll', function(){
+    	var offset = $(document).scrollTop(),
+    	opacity = 0;
+    	if (offset <= fadeStart) {
+        	opacity = 1;
+    	}
+    	else if (offset <= fadeUntil) {
+        	opacity = 1 - offset/fadeUntil;
+    	}
+    	fadeout.css('opacity',opacity).html();
+	});
 
+	$('.thumbs-img').hover(function() {
+		$(this).find('.overlay').fadeIn(400);
+	},
+	function() {
+		$(this).find('.overlay').fadeOut(400);
+	});
 });
