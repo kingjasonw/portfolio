75cd2d245143429e0ec86bbbea5c222220172ac6
Jason King <kingjason22@comcast.net>
2016-04-21 15:07:58 -0400

    more redesign updates

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ $(document).ready(function() {
 	});
 	
 	$('.fadein').fadeIn(2000);
+
+	$('#game').on('click', function() {
+		$('canvas').fadeIn('slow');
+	});
+
+	$(document).scroll(function() {
+		var y = $(this).scrollTop();
+		if (y > 20) {
+			$('#work').fadeIn('slow');
+		}
+	});
 });
+
