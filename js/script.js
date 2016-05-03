e3f41ccd8b2d0c3798a14c31b32e7940b4f12edd
Jason King <kingjason22@comcast.net>
2016-05-02 21:16:12 -0400

    img overlay and more styling

--- js/script.js
+++ js/script.js
@@ -3,25 +3,46 @@ $(document).ready(function() {
 		$('[data-toggle="tooltip"]').tooltip()
 	});
 	
+	// fade in on load
 	$('.fadein').fadeIn(2000);
 
+	// flashing glyphicon
+	setInterval(function() {
+		$('.glyphicon-hand-down').toggleClass("blueGlyphicon");
+	}, 1000);
+
+	// glyphicon stay blue on hover
+	$('#showWork').hover(function() {
+		$('.glyphicon-hand-down').css('color', 'blue');
+	}, function() {
+		$('.glyphicon-hand-down').css('color', '');
+	});
+
+	// fade in work section and scroll
 	$('#showWork').on('click', function() {
 		$('#work').fadeToggle('slow');
-		$('body').animate( {
-    		scrollTop: $("#work").offset().top -20
-		}, 1000);
+		$('body').animate({ scrollTop: $('#work').offset().top -20 }, 1000);
 	});
 
+	// image overlay on hover
+	$('.projectImg').hover(function() {
+		$(this).find('.overlay').fadeIn(400);
+	},
+	function() {
+		$(this).find('.overlay').fadeOut(400);
+	});
+
+	// fade in project descriptions and scroll
 	$('.showDescription').on('click', function() {
 		$(this).find('.hiddenDescription').fadeToggle('slow');
+		$('body').animate({ scrollTop: $(this).offset().top -20}, 1000);
 	});
 
+	// fade in game and description
 	$('.gameDescription').on('click', function() {
 		$('.hiddenGameDescription').fadeIn('slow');
-	});
-
-	$('#play').on('click', function() {
 		$('#gamediv').fadeIn('slow');
+		$('body').animate({ scrollTop: $(this).offset().top -20}, 1000);
 	});
 
 });
