9a0cfb23b1e86a041766b6302b3971f7482b65dd
Jason <kingjason222@gmail.com>
2019-11-02 11:27:56 -0400

    removed unused js

--- js/script.js
+++ js/script.js
@@ -13,37 +13,10 @@ $(document).ready(function() {
 	}, 1000);
 
 	// glyphicon stay blue on hover
-	$('#showWork').hover(function() {
-		$('.glyphicon-hand-down').css('color', 'blue');
-	}, function() {
-		$('.glyphicon-hand-down').css('color', '');
-	});
-
 	$('#resume').hover(function() {
 		$('.glyphicon-cloud-download').css('color', 'blue');
 	}, function() {
 		$('.glyphicon-cloud-download').css('color', '');
-	});
-
-	// fade in work section and scroll
-	$('#showWork').on('click', function() {
-		$('#work').fadeToggle('slow');
-		$('body').animate({ scrollTop: $('#work').offset().top -20 }, 1000);
-		
-	});
-
-	// image overlay on hover
-	$('.projectImg').hover(function() {
-		$(this).find('.overlay').fadeIn(400);
-	},
-	function() {
-		$(this).find('.overlay').fadeOut(400);
-	});
-
-	// fade in project descriptions and scroll
-	$('.showDescription').on('click', function() {
-		$(this).find('.hiddenDescription').fadeIn('slow');
-		$('body').animate({ scrollTop: $(this).offset().top -20}, 1000);
 	});
 
 });
